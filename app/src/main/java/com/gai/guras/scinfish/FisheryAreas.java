bcfbb9bfda08478fcff8abac1ccb5a1c0fb081c9
gurasees1 <gurasees@bu.edu>
2018-11-08 11:52:01 -0500

    Created FisheryAreas Custom List View

---
+++ app/src/main/java/com/gai/guras/scinfish/FisheryAreas.java
@@ -0,0 +1,13 @@
+package com.gai.guras.scinfish;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FisheryAreas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fishery_areas);
+    }
+}
